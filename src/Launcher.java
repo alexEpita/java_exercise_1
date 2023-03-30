c4e45163023052ee31423e6cc448c4f303b68a76
Alex YE <alex.ye@epita.fr>
2023-03-30 19:03:29 +0200

    partie 2 3 4 et 5, je pensais avoir push avec intelij mais ça ne marchait pas, là je push avec le terminal

--- src/Launcher.java
+++ src/Launcher.java
@@ -21,10 +21,12 @@ public class Launcher {
                 scanner.nextLine();
                 System.out.println(fibo(nombre));
             }
+            else if (mot.equals("freq")){
+                path(scanner);
+            }
             else{
                 System.out.println("Unknow command");
             }
-            System.out.println("Rentrez une nouvelle commande");
             mot = scanner.nextLine();
         }
     }
@@ -35,4 +37,17 @@ public class Launcher {
         }
     }
 
+    public static void path(Scanner scanner) {
+        System.out.println("Veuillez saisir le chemin du fichier à analyser:.");
+        Path filePath = Paths.get(scanner.nextLine());
+        try {
+            String[] words = Files.readString(filePath).replaceAll("[.\n]", " ").split(" ");
+            Map<String, Long> counter = Arrays.stream(words).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+            counter.entrySet().stream()
+                    .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+                    .limit(3).forEach((obj) -> System.out.print(obj.getKey() + " "));
+        } catch (IOException e) {
+            System.out.println("Unreadable file: " + e.getClass().getSimpleName() + " - " + e.getMessage());
+        }
+    }
 }
