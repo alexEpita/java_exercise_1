e3667c1e8c5fa2f7f05cf90d586957c7eead78e7
Alex YE <alex.ye@epita.fr>
2023-03-30 20:02:49 +0200

    partie 6

--- src/Command.java
+++ src/Command.java
@@ -1,2 +1,76 @@
-public class Command {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Scanner;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+interface Command {
+    String name();
+    boolean run(Scanner scanner);
+}
+
+class Quit implements Command {
+    @Override
+    public String name() {
+        return "quit";
+    }
+
+    @Override
+    public boolean run(Scanner scanner) {
+        return true;
+    }
+}
+
+class Fibo implements Command {
+    @Override
+    public String name() {
+        return "fibo";
+    }
+
+    private static int fib(int n) {
+        if (n <= 1) return n;
+        else return fib(n-1) + fib(n-2);
+    }
+
+    @Override
+    public boolean run(Scanner scanner) {
+        System.out.println("Veuillez saisir un indice:");
+        int n = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println(fib(n));
+        return false;
+    }
+}
+
+class Freq implements Command {
+    @Override
+    public String name() {
+        return "freq";
+    }
+
+    @Override
+    public boolean run(Scanner scanner) {
+        System.out.println("Veuillez saisir le chemin du fichier à analyser:");
+        Path filePath = Paths.get(scanner.nextLine());
+        try {
+            String[] words = Files.readString(filePath).toLowerCase().split(" ");
+            Map<String, Long> counter = Arrays.stream(words).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+            counter.entrySet().stream()
+                    .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+                    .limit(3).forEach((obj) -> System.out.print(obj.getKey() + " "));
+            System.out.println();
+        } catch (IOException e) {
+            System.out.println("Unreadable file: " + e.getClass().getSimpleName() + " - " + e.getMessage());
+        }
+        return false;
+    }
 }
--- src/Launcher.java
+++ src/Launcher.java
@@ -1,53 +1,31 @@
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.function.Function;
-import java.util.stream.Collectors;
+import java.util.*;
 public class Launcher {
-    public static void main(String[] args){
 
-        System.out.println("Bienvenue");
+    public static void main(String[] args) {
+        System.out.println("Bienvenue. Commandes : quit, fibo, freq");
+
+        List<Command> commands = new ArrayList<>();
+        commands.add(new Quit());
+        commands.add(new Fibo());
+        commands.add(new Freq());
+
         Scanner scanner = new Scanner(System.in);
-        String mot = scanner.nextLine();
-        while (!mot.equals("quit")){
-            if (mot.equals("fibo")) {
-                System.out.println("Rentrez un nopmbre pour faire fibo");
-                int nombre = scanner.nextInt();
-                scanner.nextLine();
-                System.out.println(fibo(nombre));
-            }
-            else if (mot.equals("freq")){
-                path(scanner);
-            }
-            else{
-                System.out.println("Unknow command");
-            }
-            mot = scanner.nextLine();
-        }
-    }
-    public static int fibo(int n){
-        if (n <= 1) return n;
-        else{
-            return fibo(n-1) + fibo(n-2);
-        }
-    }
 
-    public static void path(Scanner scanner) {
-        System.out.println("Veuillez saisir le chemin du fichier à analyser:.");
-        Path filePath = Paths.get(scanner.nextLine());
-        try {
-            String[] words = Files.readString(filePath).replaceAll("[.\n]", " ").split(" ");
-            Map<String, Long> counter = Arrays.stream(words).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-            counter.entrySet().stream()
-                    .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
-                    .limit(3).forEach((obj) -> System.out.print(obj.getKey() + " "));
-        } catch (IOException e) {
-            System.out.println("Unreadable file: " + e.getClass().getSimpleName() + " - " + e.getMessage());
+        boolean run = true;
+        while (run) {
+            String mot = scanner.nextLine();
+            boolean connu = false;
+            for(Command command: commands) {
+                if (command.name().equals(mot)) {
+                    connu = true;
+                    run = !command.run(scanner);
+                    break;
+                }
+            }
+            if (!connu) {
+                System.out.println("Unknown command");
+            }
         }
+        scanner.close();
     }
 }
